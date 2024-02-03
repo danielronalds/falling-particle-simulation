282a151eec7f2e4e1e9e474489b913da2381dd06
Daniel R <nzdanron@gmail.com>
2024-02-03 15:24:43 +1300

    feat: add drawing particles to stdout
    Created a function that accepts a 2d vec of booleans, drawing particles when an element is true

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,55 @@
-fn main() {
-    println!("Hello, world!");
+use crossterm::{cursor, execute, style::Color, terminal};
+use std::{io::{self, Stdout, Write}, thread};
+
+fn main() -> io::Result<()> {
+    let mut stdout = io::stdout();
+
+    let grid = vec![
+        vec![false, false, true],
+        vec![false, true, false],
+        vec![false, false, true],
+    ];
+
+    execute!(stdout, terminal::EnterAlternateScreen, cursor::Hide)?;
+
+    let _ = draw_grid(grid);
+
+    thread::sleep_ms(3000);
+
+    execute!(stdout, terminal::LeaveAlternateScreen, cursor::Show)?;
+
+
+    Ok(())
+}
+
+fn draw_grid(grid: Vec<Vec<bool>>) -> io::Result<()> {
+    let mut stdout = io::stdout();
+
+    execute!(
+        stdout,
+        cursor::MoveTo(0, 0),
+        terminal::Clear(terminal::ClearType::All)
+    )?;
+
+    for (y, row) in grid.iter().enumerate() {
+        for (x, cell) in row.iter().enumerate() {
+            if *cell {
+                draw_particle(&mut stdout, x.try_into().unwrap(), y.try_into().unwrap())?;
+            }
+        }
+    }
+
+    Ok(())
+}
+
+fn draw_particle(stdout: &mut Stdout, x: u16, y: u16) -> io::Result<()> {
+    execute!(
+        stdout,
+        cursor::MoveTo(x * 2, y),
+        crossterm::style::SetBackgroundColor(Color::White),
+        crossterm::style::Print("  "),
+        crossterm::style::SetBackgroundColor(Color::Reset),
+    )?;
+
+    Ok(())
 }
